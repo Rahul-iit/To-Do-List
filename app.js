3d878326fccc04b599559211a59214145bdc6eac
Rahul-iit <54033258+Rahul-iit@users.noreply.github.com>
2022-04-23 20:51:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const itemSchema = {
+  name: String,
+};
+const Item = mongoose.model("list", itemSchema);
+
+const item1 = new Item({
+  name: "Add Task",
+});
+
+const d = [item1];
+Item.insertMany(d, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfuly Saved!");
+  }
+});
+
+app.get("/", function (req, res) {
+  Item.find({}, function (err, f) {
+    // console.log(f);
+    if (f.length === 0) {
+      Item.insertMany(d, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully saved items to DB");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", { newListItems: f });
+    }
+  });
+});
+
+app.post("/", function (req, res) {
+  const itemName = req.body.n;
+  const item = new Item({
+    name: itemName,
+  });
+  item.save();
+  res.redirect("/");
+});
+app.post("/delete", function (req, res) {
+  const check = req.body.checkbox;
+  Item.findByIdAndRemove(check, function (err) {
+    if (!err) {
+      console.log("Successfully deleted");
+      res.redirect("/");
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Listening..");
+});
